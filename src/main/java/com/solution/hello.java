393ca17349e4b4be672c118511ee1866b973068f
sunilsyenare <syenare97@gmail.com>
2019-09-19 21:24:57 +0530

    new functionality

--- src/main/java/com/solution/hello.java
+++ src/main/java/com/solution/hello.java
@@ -2,12 +2,13 @@ package com.solution;
 
 public class hello {
 
+    public static boolean sum(){
+        return true;
+    }
     public static void main(String args[])
     {
         System.out.println("joi");
         System.out.println("hello");
-        System.out.println("Solid");
-        System.out.println("Solid");
         System.out.println("Solid");
     }
 }

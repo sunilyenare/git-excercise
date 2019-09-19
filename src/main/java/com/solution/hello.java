99912763890f783479bf196003747e9691c1cc18
sunilsyenare <syenare97@gmail.com>
2019-09-19 21:18:47 +0530

    fold

--- src/main/java/com/solution/hello.java
+++ src/main/java/com/solution/hello.java
@@ -5,9 +5,7 @@ public class hello {
     public static void main(String args[])
     {
         System.out.println("joi");
-        System.out.println("hello");
-        System.out.println("Solid");
-        System.out.println("Solid");
-        System.out.println("Solid");
+
+        System.out.println("jai jai omkar");
     }
 }
